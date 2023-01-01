9ecc0e88ae01d43fff46bcde3a6d52f43965049b
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-01-01 11:50:29 +0900

    【Fix】worksdetailレイアウト修正 / scss構成修正【1.5h】

--- single-works.php
+++ single-works.php
@@ -48,159 +48,150 @@
                 <?php the_title() ?>
             </div>
         </section>
+        <article class="p-works-detail">
+            <nav class="p-works-detail__nav">
+                <ul class="p-works-detail__list">
+                    <li class="p-works-detail__item">
+                        <a href="#item01" class="p-works-detail__link js-link01 is-active">概要</a>
+                    </li>
+                    <li class="p-works-detail__item">
+                        <a href="#item02" class="p-works-detail__link js-link02">ギャラリー</a>
+                    </li>
+                    <li class="p-works-detail__item">
+                        <a href="#item03" class="p-works-detail__link js-link03">工事内容</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="p-works-detail__content">
+                <h1 class="p-works-detail__title"><?php the_title() ?></h1>
+                <div class="p-works-detail__info">
+                    <time><?php the_time('Y.m.d') ?></time>
+                    <!-- <time>2022.02.24</time> -->
+                    <span class="p-works-detail__tag">
+                        <?php
+                        $terms = wp_get_object_terms($post->ID, 'works_category');
+                        foreach ($terms as $term) {
+                            echo  $term->name;
+                        }
+                        ?>
+                    </span>
+                </div>
+                <figure class="p-works-detail__eyecatch">
+                    <?php if (has_post_thumbnail()) :
+                        the_post_thumbnail('large'); ?>
+                    <?php
+                    else :
+                    ?>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/common/no-image.png"
+                        alt="アイキャッチ画像がありません" />
+                    <?php
+                    endif; ?>
+                </figure>
+                <div id="item01" class="p-works-detail__block js-block01">
+                    <h2 class="p-works-detail__blockTitle">概要</h2>
+                    <p class="p-works-detail__blockText">
+                        <?php the_field('outline'); ?>
+                    </p>
+                </div>
+                <div id="item02" class="p-works-detail__block js-block02">
+                    <h2 class="p-works-detail__blockTitle">ギャラリー</h2>
+                    <ul class="p-works-detail__gallery">
+                        <?php if (get_field('gallery')) : ?>
+                        <?php while (the_repeater_field('gallery')) : ?>
+                        <li class="p-works-detail__galleryItem">
+                            <a href="<?php the_sub_field('gallery_img'); ?>"
+                                class="p-works-detail__galleryLink glightbox">
+                                <figure><img src="<?php the_sub_field('gallery_img'); ?>" alt="" /></figure>
+                            </a>
+                        </li>
+                        <?php endwhile; ?>
+                        <?php endif; ?>
+                        <?php wp_reset_postdata(); ?>
+                    </ul>
+                </div>
+                <div id="item03" class="p-works-detail__block js-block03">
+                    <h2 class="p-works-detail__blockTitle">工事内容</h2>
 
-        <!-- sec-works-detail -->
-        <section class="p-works-detail">
-            <div class="l-container">
-                <article class="p-works-detail__article">
-                    <nav class="p-works-detail__nav">
-                        <ul class="p-works-detail__list">
-                            <li class="p-works-detail__item">
-                                <a href="#item01" class="p-works-detail__link js-link01 is-active">概要</a>
-                            </li>
-                            <li class="p-works-detail__item">
-                                <a href="#item02" class="p-works-detail__link js-link02">ギャラリー</a>
-                            </li>
-                            <li class="p-works-detail__item">
-                                <a href="#item03" class="p-works-detail__link js-link03">工事内容</a>
-                            </li>
-                        </ul>
-                    </nav>
-
-                    <div class="p-works-detail__content">
-                        <h1 class="p-works-detail__title"><?php the_title() ?></h1>
-                        <div class="p-works-detail__info">
-                            <time><?php the_time('Y.m.d') ?></time>
-                            <!-- <time>2022.02.24</time> -->
-                            <span class="p-works-detail__tag">
-                                <?php
-                                $terms = wp_get_object_terms($post->ID, 'works_category');
-                                foreach ($terms as $term) {
-                                    echo  $term->name;
-                                }
-                                ?>
-                            </span>
+                    <dl class="c-definition__list">
+                        <?php if (get_field('works_contents')) : ?>
+                        <?php while (the_repeater_field('works_contents')) : ?>
+                        <div class="c-definition__list-item">
+                            <dt class="c-definition__term"><?php the_sub_field('works_contents_name'); ?></dt>
+                            <dd class="c-definition__desc"><?php the_sub_field('works_contents_desc'); ?></dd>
                         </div>
-                        <figure class="p-works-detail__eyecatch">
-                            <?php if (has_post_thumbnail()) :
-                                the_post_thumbnail('large'); ?>
-                            <?php
-                            else :
-                            ?>
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/common/no-image.png"
-                                alt="アイキャッチ画像がありません" />
-                            <?php
-                            endif; ?>
-                        </figure>
-                        <div id="item01" class="p-works-detail__block js-block01">
-                            <h2 class="p-works-detail__blockTitle">概要</h2>
-                            <p class="p-works-detail__blockText">
-                                <?php the_field('outline'); ?>
-                            </p>
-                        </div>
-                        <div id="item02" class="p-works-detail__block js-block02">
-                            <h2 class="p-works-detail__blockTitle">ギャラリー</h2>
-                            <ul class="p-works-detail__gallery">
-                                <?php if (get_field('gallery')) : ?>
-                                <?php while (the_repeater_field('gallery')) : ?>
-                                <li class="p-works-detail__galleryItem">
-                                    <a href="<?php the_sub_field('gallery_img'); ?>"
-                                        class="p-works-detail__galleryLink glightbox">
-                                        <figure><img src="<?php the_sub_field('gallery_img'); ?>" alt="" /></figure>
-                                    </a>
-                                </li>
-                                <?php endwhile; ?>
-                                <?php endif; ?>
-                                <?php wp_reset_postdata(); ?>
-                            </ul>
-                        </div>
-                        <div id="item03" class="p-works-detail__block js-block03">
-                            <h2 class="p-works-detail__blockTitle">工事内容</h2>
-
-                            <dl class="c-definition__list">
-                                <?php if (get_field('works_contents')) : ?>
-                                <?php while (the_repeater_field('works_contents')) : ?>
-                                <div class="c-definition__list-item">
-                                    <dt class="c-definition__term"><?php the_sub_field('works_contents_name'); ?></dt>
-                                    <dd class="c-definition__desc"><?php the_sub_field('works_contents_desc'); ?></dd>
-                                </div>
-                                <?php endwhile; ?>
-                                <?php endif; ?>
-                                <?php wp_reset_postdata(); ?>
-                            </dl>
-                        </div>
-
-                        <div class="p-works-detail__otherLinkInner">
-                            <ul class="p-works-detail__otherLinkList">
-                                <li class="p-works-detail__otherLinkItem">
-                                    <a href="<?php echo esc_url(home_url('works_category/archi')); ?>"
-                                        class="p-works-detail__otherLink js-works-cursor01 -cursorNone -archi">
-                                        <div class="p-works-detail__item-inner">
-                                            <div class="p-works-detail__text-left">
-                                                <h3 class="p-works-detail__item-title">
-                                                    Architecture
-                                                    <br />
-                                                    <span>建築事業</span>
-                                                </h3>
-                                            </div>
-                                            <div class="p-works-detail__text-right">
-                                                <div class="c-arrow c-arrow--round"></div>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li class="p-works-detail__otherLinkItem">
-                                    <a href="<?php echo esc_url(home_url('works_category/civil')); ?>"
-                                        class="p-works-detail__otherLink js-works-cursor02 -cursorNone -civil">
-                                        <div class="p-works-detail__item-inner">
-                                            <div class="p-works-detail__text-left">
-                                                <h3 class="p-works-detail__item-title">
-                                                    Civil engineering
-                                                    <br />
-                                                    <span>土木事業</span>
-                                                </h3>
-                                            </div>
-                                            <div class="p-works-detail__text-right">
-                                                <div class="c-arrow c-arrow--round"></div>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li class="p-works-detail__otherLinkItem">
-                                    <a href="<?php echo esc_url(home_url('works_category/housing')); ?>"
-                                        class="p-works-detail__otherLink js-works-cursor03 -cursorNone -housing">
-                                        <div class="p-works-detail__item-inner">
-                                            <div class="p-works-detail__text-left">
-                                                <h3 class="p-works-detail__item-title">
-                                                    Housing
-                                                    <br />
-                                                    <span>住宅事業</span>
-                                                </h3>
-                                            </div>
-                                            <div class="p-works-detail__text-right">
-                                                <div class="c-arrow c-arrow--round"></div>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="p-works-detail__btn-wrapper">
-                            <button class="p-works-detail__btn">
-                                <a href=""> 施工実績一覧へ </a>
-                            </button>
-                        </div>
-                        <!-- <div class="c-list-button__block">
-                  <a href="<%= path %>works" class="c-list-button"
-                    >施工実績一覧へ</a
-                  >
-                </div> -->
-                    </div>
-                </article>
+                        <?php endwhile; ?>
+                        <?php endif; ?>
+                        <?php wp_reset_postdata(); ?>
+                    </dl>
+                </div>
             </div>
+        </article>
+        <section class="p-works-detail-otherlink">
+            <div class="p-works-detail-otherlink__inner">
+                <ul class="p-works-detail-otherlink__list">
+                    <li class="p-works-detail-otherlink__item">
+                        <a href="<?php echo esc_url(home_url('works_category/archi')); ?>"
+                            class="p-works-detail-otherlink__link js-works-cursor01 -cursorNone -archi">
+                            <div class="p-works-detail-otherlink__item-inner">
+                                <div class="p-works-detail-otherlink__text-left">
+                                    <h3 class="p-works-detail-otherlink__item-title">
+                                        Architecture
+                                        <br />
+                                        <span>建築事業</span>
+                                    </h3>
+                                </div>
+                                <div class="p-works-detail-otherlink__text-right">
+                                    <div class="c-arrow c-arrow--round"></div>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li class="p-works-detail-otherlink__item">
+                        <a href="<?php echo esc_url(home_url('works_category/civil')); ?>"
+                            class="p-works-detail-otherlink__link js-works-cursor02 -cursorNone -civil">
+                            <div class="p-works-detail-otherlink__item-inner">
+                                <div class="p-works-detail-otherlink__text-left">
+                                    <h3 class="p-works-detail-otherlink__item-title">
+                                        Civil engineering
+                                        <br />
+                                        <span>土木事業</span>
+                                    </h3>
+                                </div>
+                                <div class="p-works-detail-otherlink__text-right">
+                                    <div class="c-arrow c-arrow--round"></div>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li class="p-works-detail-otherlink__item">
+                        <a href="<?php echo esc_url(home_url('works_category/housing')); ?>"
+                            class="p-works-detail-otherlink__link js-works-cursor03 -cursorNone -housing">
+                            <div class="p-works-detail-otherlink__item-inner">
+                                <div class="p-works-detail-otherlink__text-left">
+                                    <h3 class="p-works-detail-otherlink__item-title">
+                                        Housing
+                                        <br />
+                                        <span>住宅事業</span>
+                                    </h3>
+                                </div>
+                                <div class="p-works-detail-otherlink__text-right">
+                                    <div class="c-arrow c-arrow--round"></div>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="p-works-detail-otherlink__btn-wrapper">
+                <button class="p-works-detail-otherlink__btn">
+                    <a href="<?php echo esc_url(home_url('works')); ?>"> 施工実績一覧へ </a>
+                </button>
+            </div>
+
+
         </section>
-        <!-- /sec-works-detail -->
-        <section class="p-works-detail-otherlink"></section>
     </main>
     <!-- /main -->
 
