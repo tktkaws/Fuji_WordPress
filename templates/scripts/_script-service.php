1e50caaf94d98bc4d2544c2772c87f51ebba18ac
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-01-07 07:41:42 +0900

    【Fix】不要ファイルの削除【0.5h】

--- templates/scripts/_script-service.php
+++
@@ -1,2 +0,0 @@
-<!-- service.bundle.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/service.bundle.js"></script>
--- templates/scripts/_script-works.php
+++
@@ -1,2 +0,0 @@
-<!-- works.bundle.js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/works.bundle.js"></script>
