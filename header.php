9e21be263883afc11af4b82957ac24980b07ddb3
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-01-08 07:30:20 +0900

    【Fix】OGP設定・headタグの構成変更【0.5h】

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="robots" content="noindex" />
-<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon.ico" />
-<link rel="apple-touch-icon" sizes="180x180"
-    href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/apple-touch-icon.png" />
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet" />
-<?php wp_head(); ?>
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/ fb# website: http://ogp.me/ns/website#">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex" />
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180"
+        href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/apple-touch-icon.png" />
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet" />
+    <?php wp_head(); ?>
+</head>
--- page-company.php
+++ page-company.php
@@ -1,11 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
-
-<head>
-
-    <?php get_header(); ?>
-
-</head>
+<?php get_header(); ?>
 
 <body class="subpage">
     <?php get_template_part('templates/_l-header'); ?>
@@ -157,6 +152,64 @@
     <?php get_footer(); ?>
     <!-- APIを利用するためのライブラリ（CDN）の読み込み -->
     <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyDtqCTltRy0JWgDs68gDj8LA3CBcEwVdyM&callback=initMap" async>
+    </script>
+    <!-- マーカーをオリジナル画像にするためのスクリプト -->
+    <script>
+    if (window.matchMedia("(max-width: 767px)").matches) {
+        //スマホ処理
+        function initMap() {
+            var mapPosition = {
+                lat: 35.64828,
+                lng: 140.03468
+            };
+            var mapArea = document.getElementById("maps");
+            var mapOptions = {
+                center: mapPosition,
+                zoom: 14,
+            };
+            var map = new google.maps.Map(mapArea, mapOptions);
+
+            var markerOptions = {
+                map: map,
+                position: mapPosition,
+
+                icon: new google.maps.MarkerImage(
+                    "/assets/images/common/map_icon_sp.png",
+                    new google.maps.Size(100, 66),
+                    new google.maps.Point(0, 0),
+                    new google.maps.Point(50, 50)
+                ),
+            };
+            var marker = new google.maps.Marker(markerOptions);
+        }
+    } else if (window.matchMedia("(min-width:769px)").matches) {
+        //PC処理
+        function initMap() {
+            var mapPosition = {
+                lat: 35.64828,
+                lng: 140.03468
+            };
+            var mapArea = document.getElementById("maps");
+            var mapOptions = {
+                center: mapPosition,
+                zoom: 16,
+            };
+            var map = new google.maps.Map(mapArea, mapOptions);
+
+            var markerOptions = {
+                map: map,
+                position: mapPosition,
+
+                icon: new google.maps.MarkerImage(
+                    "/assets/images/common/map_icon_pc.png",
+                    new google.maps.Size(162, 107),
+                    new google.maps.Point(0, 0),
+                    new google.maps.Point(80, 50)
+                ),
+            };
+            var marker = new google.maps.Marker(markerOptions);
+        }
+    }
     </script>
 </body>
 
